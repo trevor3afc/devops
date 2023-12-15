e9e81dea7a8cb3d59041816782fbab44bcff3202
cosine <cosine@pc-steel>
2023-12-16 03:30:20 +0800

    update

--- executor.sh
+++ executor.sh
@@ -2,9 +2,17 @@
 
 module=$1
 feature=$2
-argsArray=("$@")
+argsArray="$@"
 
 gitHubUrl="https://raw.githubusercontent.com/trevor3afc/devops/main/$module/$feature.sh"
 
 echo "gitHubUrl: $gitHubUrl"
-echo "argsArray: $argsArray"
+
+
+# remove first two args
+argsArray=("${argsArray[@]:2}")
+
+for arg in "${argsArray[@]}"; do
+    echo "arg: $arg"
+    args="$args $arg"
+done
